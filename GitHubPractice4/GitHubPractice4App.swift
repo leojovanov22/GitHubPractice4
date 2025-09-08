7eb40857f113aa1c4aaade849878394cb90d3529
Leo Jovanov <leojovanov22@gmail.com>
2025-09-08 10:49:15 -0500

    Initial Commit

---
+++ GitHubPractice4/GitHubPractice4App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPractice4App.swift
+//  GitHubPractice4
+//
+//  Created by Leonardo Jovanov on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPractice4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubPractice4Tests/GitHubPractice4Tests.swift
@@ -0,0 +1,16 @@
+//
+//  GitHubPractice4Tests.swift
+//  GitHubPractice4Tests
+//
+//  Created by Leonardo Jovanov on 9/8/25.
+//
+
+import Testing
+
+struct GitHubPractice4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
