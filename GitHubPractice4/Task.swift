520ce872cc94ee3f59e761247b167c5578190371
Aadi Bilfaqi <abilfaqi7040@stu.d214.org>
2025-09-08 11:03:45 -0500

    I created a struct for Task. I also created a variable called nameOfTask, which is a string.

---
+++ GitHubPractice4/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  GitHubPractice4.swiftpm
+//
+//  Created by Aadi D. Bilfaqi on 9/8/25.
+//
+import SwiftUI
+
+@main
+struct Task: App {
+    @State var nameOfTask: String = ""
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
